791dc6189ba2daacbfa1164d6614daf8f390768a
Wildan Yazid Ziddan <wildanziddan28@gmail.com>
2025-06-11 09:07:00 +0700

    Add project files.

---
+++ Login.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Karaoke_PopRock
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.tfUsername = new System.Windows.Forms.TextBox();
+            this.tfPassword = new System.Windows.Forms.TextBox();
+            this.btnMasuk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tfUsername
+            // 
+            this.tfUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tfUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tfUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tfUsername.Location = new System.Drawing.Point(842, 402);
+            this.tfUsername.Name = "tfUsername";
+            this.tfUsername.Size = new System.Drawing.Size(358, 31);
+            this.tfUsername.TabIndex = 0;
+            // 
+            // tfPassword
+            // 
+            this.tfPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tfPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tfPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tfPassword.Location = new System.Drawing.Point(842, 527);
+            this.tfPassword.Name = "tfPassword";
+            this.tfPassword.PasswordChar = '*';
+            this.tfPassword.Size = new System.Drawing.Size(358, 31);
+            this.tfPassword.TabIndex = 1;
+            // 
+            // btnMasuk
+            // 
+            this.btnMasuk.BackColor = System.Drawing.SystemColors.Window;
+            this.btnMasuk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnMasuk.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMasuk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMasuk.ForeColor = System.Drawing.Color.Transparent;
+            this.btnMasuk.Location = new System.Drawing.Point(824, 624);
+            this.btnMasuk.Name = "btnMasuk";
+            this.btnMasuk.Size = new System.Drawing.Size(392, 75);
+            this.btnMasuk.TabIndex = 2;
+            this.btnMasuk.UseMnemonic = false;
+            this.btnMasuk.UseVisualStyleBackColor = false;
+            this.btnMasuk.Click += new System.EventHandler(this.btnMasuk_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1440, 1024);
+            this.ControlBox = false;
+            this.Controls.Add(this.tfPassword);
+            this.Controls.Add(this.tfUsername);
+            this.Controls.Add(this.btnMasuk);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Login";
+            this.Text = "Karaoke PopRock";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tfUsername;
+        private System.Windows.Forms.TextBox tfPassword;
+        private System.Windows.Forms.Button btnMasuk;
+    }
+}
+
---
+++ Login.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace Karaoke_PopRock
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            //Untuk set button login jadi transparan/tidak terlihat
+            btnMasuk.FlatStyle = FlatStyle.Flat;
+            btnMasuk.FlatAppearance.BorderSize = 0;
+            btnMasuk.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnMasuk.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnMasuk.BackColor = Color.Transparent;
+        }
+
+        private String AuthenticateUser(string username, string password)
+        {
+            string connectionString = "Server=.;Database=Db_karaoke_poprock;User Id=Wyzord;Password=wyzord2006;";
+            string role = null;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand("sp_AuthenticateUser", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                // Add the OUTPUT parameter
+                SqlParameter roleParam = new SqlParameter("@role", SqlDbType.NVarChar, 50)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                cmd.Parameters.Add(roleParam);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+
+                // Get the value from the output parameter
+                if (roleParam.Value != DBNull.Value)
+                {
+                    role = roleParam.Value.ToString();
+                }
+            }
+
+            return role;
+        }
+
+        private void btnMasuk_Click(object sender, EventArgs e)
+        {
+            string username = tfUsername.Text.Trim();
+            string password = tfPassword.Text.Trim();
+
+            string role = AuthenticateUser(username, password);
+            if (role != null)
+            {
+                MessageBox.Show("Login berhasil!", "INFORMATION", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                if (role == "Admin")
+                {
+                    //new AdminDashboard().Show();
+                }
+                else if (role == "Kasir")
+                {
+                    new Kasir().Show();
+                }
+
+                this.Hide();
+            }
+
+            else if (string.IsNullOrEmpty(tfUsername.Text) && string.IsNullOrEmpty(tfPassword.Text))
+            {
+                MessageBox.Show("Input tidak boleh kosong", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            else if (string.IsNullOrEmpty(tfUsername.Text))
+            {
+                MessageBox.Show("Username tidak boleh kosong", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else if ( string.IsNullOrEmpty(tfPassword.Text))
+            {
+                MessageBox.Show("Password tidak boleh kosong", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else if (username != "@username" || password != "@password")
+            {
+                MessageBox.Show("Username atau password yang dimasukkan salah", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
+    }
+}
